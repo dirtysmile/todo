19c91db1c6c85f6be0b8aac70f09f09267e716b3
thkim <dirtysmile89@naver.com>
2020-03-25 08:17:29 +0900

    2020-03-25 작업

---
+++ todo-list/src/components/TodoInsert.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { MdAdd } from 'react-icon/md';
+
+const TodoInsert = () => {
+  return (
+    <from className="TodoInsert">
+      <input placeholder="할 일을 입력하세요" />
+      <button type="submit"></button>
+    </from>
+  );
+};
+
+export default TodoInsert;
